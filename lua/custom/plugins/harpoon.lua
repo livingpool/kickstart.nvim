7a4674179ecb0688bf5c2960022d8d9bbac32007
Livingpool <52132459+Livingpool@users.noreply.github.com>
2024-08-30 21:30:59 +0800

    add basic harpoon config

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,43 @@
+-- ThePrimeagen: harpoon
+-- https://github.com/ThePrimeagen/harpoon/tree/harpoon2
+
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  -- Basic telescope configuration (using telescope as UI)
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup {}
+
+    -- Basic keymaps
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end)
+    vim.keymap.set('n', '<C-e>', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    vim.keymap.set('n', '<C-h>', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<C-t>', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<C-n>', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', '<C-s>', function()
+      harpoon:list():select(4)
+    end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<C-S-P>', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<C-S-N>', function()
+      harpoon:list():next()
+    end)
+  end,
+}
