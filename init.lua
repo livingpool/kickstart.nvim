fcf744e9324ab0390bc4edff838e31332dcd8964
Tim Liu(劉庭瑋) <tim_liu@t-01014887ml.htctaipei.htc.com.tw>
2024-10-08 13:42:37 +0800

    add lsp: nvim-metals

--- init.lua
+++ init.lua
@@ -571,7 +571,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
-        pyright = {},
+        -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -579,11 +579,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
         --
-        html = {},
-        css_variables = {},
-        cssls = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -795,32 +791,70 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rose-pine/neovim',
+    'catppuccin/nvim',
     lazy = false, -- Make sure to load this during startup if this is your main colorscheme
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      require('rose-pine').setup {
-        variant = 'auto', -- auto, main, moon, or dawn
-        dark_variant = 'main', -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
-
-        enable = {
-          terminal = true,
-          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true, -- Handle deprecated options automatically
+      require('catppuccin').setup {
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'mocha',
         },
-
-        styles = {
-          bold = false,
-          italic = false,
-          transparency = true,
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = true, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' }, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {
+          mocha = {
+            base = '#000000',
+            mantle = '#000000',
+            crust = '#000000',
+          },
+        },
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
       }
-      vim.cmd.colorscheme 'rose-pine'
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -888,6 +922,8 @@ require('lazy').setup({
         'vim',
         'vimdoc',
         'go',
+        'scala',
+        'java',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
---
+++ lua/custom/plugins/metals.lua
@@ -0,0 +1,155 @@
+-- nvim-metals is a Lua plugin built to provide a better experience while using Metals, the Scala Language Server, with Neovim's built-in LSP support
+-- https://github.com/scalameta/nvim-metals?tab=readme-ov-file
+return {
+  'scalameta/nvim-metals',
+  ft = { 'scala', 'sbt', 'java' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'j-hui/fidget.nvim',
+      opt = {},
+    },
+    {
+      'mfussenegger/nvim-dap',
+      config = function(self, opts)
+        -- Debug settings if you're using nvim-dap
+        local dap = require 'dap'
+
+        dap.configurations.scala = {
+          {
+            type = 'scala',
+            request = 'launch',
+            name = 'RunOrTest',
+            metals = {
+              runType = 'runOrTestFile',
+              --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+            },
+          },
+          {
+            type = 'scala',
+            request = 'launch',
+            name = 'Test Target',
+            metals = {
+              runType = 'testTarget',
+            },
+          },
+        }
+      end,
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "<leader>cW", function () require('metals').hover_worksheet() end, desc = "Metals Worksheet" },
+    { "<leader>cM", function () require('telescope').extensions.metals.commands() end, desc = "Telescope Metals Commands" },
+  },
+  init = function()
+    local metals_config = require('metals').bare_config()
+
+    -- Example of settings
+    metals_config.settings = {
+      showImplicitArguments = true,
+      excludedPackages = { 'akka.actor.typed.javadsl', 'com.github.swagger.akka.javadsl' },
+    }
+
+    -- *READ THIS*
+    -- I *highly* recommend setting statusBarProvider to either "off" or "on"
+    --
+    -- "off" will enable LSP progress notifications by Metals and you'll need
+    -- to ensure you have a plugin like fidget.nvim installed to handle them.
+    --
+    -- "on" will enable the custom Metals status extension and you *have* to have
+    -- a have settings to capture this in your statusline or else you'll not see
+    -- any messages from metals. There is more info in the help docs about this
+    metals_config.init_options.statusBarProvider = 'off'
+
+    -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+    metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    metals_config.on_attach = function(client, bufnr)
+      require('metals').setup_dap()
+
+      -- LSP mappings
+      -- vim.keymap.set('n', 'gD', vim.lsp.buf.definition)
+      -- vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+      -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+      -- vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+      -- vim.keymap.set('n', 'gds', vim.lsp.buf.document_symbol)
+      -- vim.keymap.set('n', 'gws', vim.lsp.buf.workspace_symbol)
+      -- vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run)
+      -- vim.keymap.set('n', '<leader>sh', vim.lsp.buf.signature_help)
+      -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+      -- vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+      -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+
+      vim.keymap.set('n', '<leader>ws', function()
+        require('metals').hover_worksheet()
+      end)
+
+      -- all workspace diagnostics
+      vim.keymap.set('n', '<leader>aa', vim.diagnostic.setqflist)
+
+      -- all workspace errors
+      vim.keymap.set('n', '<leader>ae', function()
+        vim.diagnostic.setqflist { severity = vim.diagnostic.severity.E }
+      end)
+
+      -- all workspace warnings
+      vim.keymap.set('n', '<leader>aw', function()
+        vim.diagnostic.setqflist { severity = vim.diagnostic.severity.W }
+      end)
+
+      -- buffer diagnostics only
+      vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist)
+
+      vim.keymap.set('n', '[c', function()
+        vim.diagnostic.goto_prev { wrap = false }
+      end)
+
+      vim.keymap.set('n', ']c', function()
+        vim.diagnostic.goto_next { wrap = false }
+      end)
+
+      -- Example vim.keymap.setpings for usage with nvim-dap. If you don't use that, you can
+      -- skip these
+      vim.keymap.set('n', '<leader>dc', function()
+        require('dap').continue()
+      end)
+
+      vim.keymap.set('n', '<leader>dr', function()
+        require('dap').repl.toggle()
+      end)
+
+      vim.keymap.set('n', '<leader>dK', function()
+        require('dap.ui.widgets').hover()
+      end)
+
+      vim.keymap.set('n', '<leader>dt', function()
+        require('dap').toggle_breakpoint()
+      end)
+
+      vim.keymap.set('n', '<leader>dso', function()
+        require('dap').step_over()
+      end)
+
+      vim.keymap.set('n', '<leader>dsi', function()
+        require('dap').step_into()
+      end)
+
+      vim.keymap.set('n', '<leader>dl', function()
+        require('dap').run_last()
+      end)
+    end
+
+    return metals_config
+  end,
+  config = function(self, metals_config)
+    local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = self.ft,
+      callback = function()
+        require('metals').initialize_or_attach(metals_config)
+      end,
+      group = nvim_metals_group,
+    })
+  end,
+}
