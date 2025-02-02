4caa770a227d37059e60e6c6d9be558bbadd9157
Livingpool <52132459+Livingpool@users.noreply.github.com>
2025-02-02 09:41:47 +0800

    javascript crap

--- init.lua
+++ init.lua
@@ -644,7 +644,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
         html = {
           filetypes = { 'template', 'html', 'tmpl', 'templ' },
