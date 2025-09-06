eab5c0d64bb90550faa70dbc4fd088576fb55267
livingpool <52132459+livingpool@users.noreply.github.com>
2025-09-06 10:43:42 +0800

    add autocmds for disabling autoformat

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,5 +1,7 @@
 -- Lightweight yet powerful formatter plugin for Neovim
 -- https://github.com/stevearc/conform.nvim
+--
+-- disabling autoformat: https://github.com/stevearc/conform.nvim/issues/192
 
 return { -- Autoformat
   'stevearc/conform.nvim',
@@ -14,10 +16,43 @@ return { -- Autoformat
       mode = '',
       desc = '[F]ormat buffer',
     },
+    {
+      '<leader>tf',
+      function()
+        -- If autoformat is currently disabled for this buffer,
+        -- then enable it, otherwise disable it
+        if vim.b.disable_autoformat then
+          vim.cmd 'FormatEnable'
+          vim.notify 'Enabled autoformat for current buffer'
+        else
+          vim.cmd 'FormatDisable!'
+          vim.notify 'Disabled autoformat for current buffer'
+        end
+      end,
+      desc = 'Toggle autoformat for current buffer',
+    },
+    {
+      '<leader>tF',
+      function()
+        -- If autoformat is currently disabled globally,
+        -- then enable it globally, otherwise disable it globally
+        if vim.g.disable_autoformat then
+          vim.cmd 'FormatEnable'
+          vim.notify 'Enabled autoformat globally'
+        else
+          vim.cmd 'FormatDisable'
+          vim.notify 'Disabled autoformat globally'
+        end
+      end,
+      desc = 'Toggle autoformat globally',
+    },
   },
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
@@ -34,8 +69,8 @@ return { -- Autoformat
       }
     end,
     formatters_by_ft = {
-      c = { 'my_formatter' },
-      cpp = { 'my_formatter' },
+      c = { 'custom_clang_formatter' },
+      cpp = { 'custom_clang_formatter' },
       lua = { 'stylua' },
       sh = { 'shfmt' },
       -- Conform can also run multiple formatters sequentially
@@ -57,7 +92,7 @@ return { -- Autoformat
       prettier = {
         append_args = { '--tab-width', '2' },
       },
-      my_formatter = {
+      custom_clang_formatter = {
         command = 'clang-format',
         args = '--style="{BasedOnStyle: llvm, IndentWidth: 4, AllowShortFunctionsOnASingleLine: Empty, ColumnLimit: 120}"',
       },
@@ -66,4 +101,27 @@ return { -- Autoformat
       },
     },
   },
+  config = function(_, opts)
+    require('conform').setup(opts)
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- :FormatDisable! disables autoformat for this buffer only
+        vim.b.disable_autoformat = true
+      else
+        -- :FormatDisable disables autoformat globally
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true, -- allows the ! variant
+    })
+
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+  end,
 }
